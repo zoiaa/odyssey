50b4c8983fb5f4d3c27309d31b339b4c5e14a4f3
Audrey Brethome <audrey.brethome@gmail.com>
2019-07-03 11:30:02 +0200

    routes

---
+++ back/App.js
@@ -0,0 +1,33 @@
+const  http  =  require('http');
+const  path  =  require('path');
+const  express  =  require('express');
+const  bodyParser  =  require('body-parser');
+const  morgan  =  require('morgan');
+const  app  =  express();
+const authRouter = require('./routes/auth/auth');
+
+
+// je configure l'application
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended:  false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname  +  '/public'));
+app.use('/auth', authRouter);
+
+
+
+// j'implémente la partie API
+app.get("/", (req,res) => {
+    res.send("youhou");
+})
+/// dans le cas d'une route non trouvée, je retourne le code 404 'Not Found'
+app.use(function(req, res, next) {
+    var  err  =  new  Error('Not Found');
+    err.status  =  404;
+    next(err);
+});
+
+//je lance le serveur node
+let  server  =  app.listen( process.env.PORT  ||  3000, function(){
+    console.log('Listening on port '  +  server.address().port);
+});
